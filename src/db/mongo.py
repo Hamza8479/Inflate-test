c9e37ebaa659fb183e4b6ca37616c3e058fb0f31
hamza <hamzazaman500@gmail.com>
2026-02-05 21:24:42 +0100

    Task 7: stabilize resource usage and prevent memory leaks

--- src/db/mongo.py
+++ src/db/mongo.py
@@ -1,12 +1,22 @@
+
 from motor.motor_asyncio import AsyncIOMotorClient
 import os
 
 MONGO_URL = os.getenv("MONGO_URL", "mongodb://mongodb:27017")
 DB_NAME = "support_saas"
 
+_client = None
+_db = None
+
 async def get_db():
     """
-    Returns a database instance.
+    Returns a singleton MongoDB database instance.
     """
-    client = AsyncIOMotorClient(MONGO_URL)
-    return client[DB_NAME]
+    global _client, _db
+
+    if _client is None:
+        print("=====Creating MongoDB client====")
+        _client = AsyncIOMotorClient(MONGO_URL)
+        _db = _client[DB_NAME]
+
+    return _db
